b21a38e0f07190703b31df152ad40858c991d71e
Abigiya <abiguermias@gmail.com>
2026-07-08 14:12:09 +0300

    Fix React routing and restore frontend functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Patients from "./pages/Patients";
 import Doctors from "./pages/Doctors";
 import Appointments from "./pages/Appointments";
 
+
 function App() {
+
   return (
     <BrowserRouter>
+
       <Routes>
+
         <Route path="/" element={<Login />} />
+
         <Route path="/dashboard" element={<Dashboard />} />
+
         <Route path="/patients" element={<Patients />} />
+
         <Route path="/doctors" element={<Doctors />} />
+
         <Route path="/appointments" element={<Appointments />} />
+
       </Routes>
+
     </BrowserRouter>
   );
 }
