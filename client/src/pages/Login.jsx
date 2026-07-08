c3dfc26066f934180e01dec85b616a936133bac3
Abigiya <abiguermias@gmail.com>
2026-07-08 12:39:22 +0300

    Build React frontend and connect login to backend API

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import API from "../api/api";
+
+function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e) => {
+    e.preventDefault();
+
+    console.log("Login button clicked");
+
+    try {
+        const response = await API.post("/auth/login", {
+            email,
+            password
+        });
+
+        console.log(response.data);
+
+    } catch (error) {
+    console.log("Full Error:", error);
+    console.log("Response:", error.response);
+    console.log("Data:", error.response?.data);
+    console.log("Message:", error.message);
+}
+};
+
+    return (
+        <div>
+            <h1>MedLink Ethiopia</h1>
+            <h2>Login</h2>
+
+            <form onSubmit={handleLogin}>
+
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <br />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <br />
+
+                <button type="submit" onClick={() => console.log("button pressed")}>
+    Login
+</button>
+
+            </form>
+        </div>
+    );
+}
+
+export default Login;
